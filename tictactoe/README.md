b991f124a50f51a8be80abe7c962ab6b2c602f01
Baldbutgold <abdouplayz2003@gmail.com>
2021-10-14 20:57:55 +0100

    Basic Terminal TicTacToe
    Took me about 2 or 3 hours

---
+++ tictactoe/README.md
@@ -0,0 +1,11 @@
+# Tic Tac Toe
+
+## Description
+Basic Tic Tac Toe played in terminal, you can choose to play against a basic bot or a friend of yours
+The basic bot just picks a random number for the possible plays
+
+## Future TO DO
+- make the last check in if no possible moves means draw
+- make a web app
+- make this in javascript
+
---
+++ tictactoe/gameLogic.py
@@ -0,0 +1,91 @@
+import random
+
+playablePositions = [0,2,4,6,8,10,12,14,16]
+gameMap = ['_','|','_','|','_','\n','_','|','_','|','_','\n',' ','|',' ','|',' ','\n']
+
+def printMap():
+    print(''.join(gameMap))
+
+def startGame():
+    print("""To Play the Game Pick a value from 1 to 9
+Each value is represnted from left to right, up to bottom
+Meaning 1 is top left corner, 2 is top middle and so on
+Have Fun!!!""")
+    printMap()
+    isAlive = True
+    playerTurn()
+
+def isAlive():
+    #checks if x or o are in any of the winning positions
+    winningPositions = [[0,2,4],[6,8,10],[12,14,16],[0,6,12],[2,8,14],[4,10,16],[0,8,16],[4,8,12]]
+    for i in winningPositions:
+        x = ''
+        for j in range(3):
+            y = i[j]
+            x += gameMap[y]
+        if x == 'xxx': 
+            print('First player WON!')
+            return False
+        if x == "ooo":
+            print('Second player WON!')
+            return False
+    all_playablePositions = ''
+    for i in playablePositions:   
+        pass
+        #check if all playablepositions are an empty string
+    else: 
+        return True
+
+def gameRender(pos, sym):
+    while isAlive() == True:
+        gameMap[pos] = sym
+        deletepos = int(pos)/2
+        playablePositions[int(deletepos)] = ''
+        printMap()
+        break
+
+def checker(Player):
+    #checks for positions that are available if chosen a not avaialbe position repeat
+    available = False
+    while available == False and isAlive() == True:
+        try:
+            if Player == 'First player':
+                symbol = 'x'
+            elif Player == 'Second player':
+                symbol = 'o'
+            playedPosition = input('Pick a position from 1 to 9\n')
+            position = (int(playedPosition)  - 1) * 2
+            playedPosition = print(Player + ' Turn')
+            if position in playablePositions:
+               gameRender(position, symbol)   
+               break
+            else:
+                print('That position is unplayable')
+                pass
+        except:
+            print('Invalid Input')
+def playerTurn():
+    player = input('Press 1 to play against a friend, Press 2 to play against a basic bot\n')
+    while isAlive() == True:
+        if player == '1':
+            #start playing against a friend
+            checker('First player')
+            checker('Second player')
+        elif player == '2':
+            #start playing against a bot
+            checker('First player')
+            basicBot()
+        else:
+            print('Invalid Input, try again')
+            quit()
+
+def basicBot(): 
+    randomposition = random.choice(playablePositions)
+    symbol = 'o'
+    while randomposition == '':
+        randomposition = random.choice(playablePositions)
+    else:
+        gameRender(randomposition , symbol)
+        print("Bot's turn he played", (int(randomposition/2 + 1)))
+            
+startGame()
